7d57422a4a0c62ab8bcfc52ac91c1efc19296b7f
Scott Williams <ScottWilliams220608@gmail.com>
2023-05-23 09:31:04 -0400

    answers for lesson 9

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        minlength: 3,
+        maxlength: 25,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide a email'],
+       match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/],
+       unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8,
+        maxlength: 12
+    }
+})
+
+UserSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    
+})
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({userId:this._id,name:this.name}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME,})
+}
+
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+    const isMatch = await bcrypt.compare(canditatePassword, this.password)
+    return isMatch
+}
+
+
+module.exports = mongoose.model('User', UserSchema)
---
+++ routes/jobs.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const {getAllAccounts, getAccount, createAccount, updateAccount, deleteAccount} = require('../controllers/jobs')
+
+router.route('/').post(createAccount).get(getAllAccounts)
+router.route('/:id').get(getAccount).delete(deleteAccount).patch(updateAccount)
+
+module.exports = router
