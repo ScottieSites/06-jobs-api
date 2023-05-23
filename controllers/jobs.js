02b363f4817b51b9c050a1ecbe68420ec81fa1bf
Scott Williams <ScottWilliams220608@gmail.com>
2023-05-23 13:45:58 -0400

    answers for lesson 10

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,35 +1,72 @@
-const Job = require('../models/Job')
-const {StatusCodes} = require('http-status-codes')
-const {BadRequestError, NotFoundError} = require('../errors')
+const Account = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllAccounts = async (req, res) => {
-    res.send('get all accounts')
-}
+  const accounts = await Job.find({ createdBy: req.user.userId }).sort(
+    "createdAt"
+  );
+  res.status(StatusCodes.OK).json({ accounts, count: accounts.length });
+};
 
 const getAccount = async (req, res) => {
-    res.send('get account')
-}
+  const {
+    user: { userId },
+    params: { id: accountId },
+  } = req;
+
+  const account = await Account.findOne({
+    _id: accountId,
+    createdBy: userId,
+  });
+  if (!account) {
+    throw new NotFoundError(`Account not found ${accountId}`);
+  }
+  res.status(StatusCodes.OK).json({ account });
+};
 
 const createAccount = async (req, res) => {
-    req.body.createdBy = req.user.userID
-    const account = await Account.create(req.body)
-    res.status(StatusCodes.CREATED).json({account})
-}
+  req.body.createdBy = req.user.userID;
+  const account = await Account.create(req.body);
+  res.status(StatusCodes.CREATED).json({ account });
+};
 
 const updateAccount = async (req, res) => {
-    res.send('create account')
-}
+const {
+    body:{company, position},
+ user: { userId },
+    params: { id: accountId },
+        } = req;
+    if(company === '' || position === '') {
+        throw new BadRequestError("Please add company and position");
+    }    
+ const account = await Account.findByIdAndUpdate({_id:accountId,createdBy:userId}, req.body, {new:true, runValidators: true});
+ if (!account) {
+    throw new NotFoundError(`Account not found ${accountId}`);
+  }
+  res.status(StatusCodes.OK).json({ account });
+
+};
 
 const deleteAccount = async (req, res) => {
-    res.send('create account')
-}
-
-
+    const {
+        user: { userId },
+        params: { id: accountId },
+    } = req;
+    const account = await Account.findByIdAndRemove({
+    _id:accountId,
+    createdBy: userId,
+    })
+    if (!account) {
+        throw new NotFoundError(`Account not found ${accountId}`);
+    }
+    res.status(StatusCodes.OK).json({ account });
+};
 
 module.exports = {
-    getAllAccounts,
-    getAccount,
-    createAccount,
-    updateAccount,
-    deleteAccount
-}
+  getAllAccounts,
+  getAccount,
+  createAccount,
+  updateAccount,
+  deleteAccount,
+};
